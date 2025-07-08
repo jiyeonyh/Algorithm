25a256e6dd1ecc437af14bf07fdfbbc6f7547aa9
jiyeonyh <56222489+jiyeonyh@users.noreply.github.com>
2025-07-08 15:11:43 +0900

    Update 미로 탐색.py

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -1,36 +1,54 @@
 from collections import deque
+import copy
 
-n, m = map(int, input().split())
-board = []
-dx = [1, 0, -1, 0]
-dy = [0, 1, 0, -1]
+def read_input():
+    n, m = map(int, input().split())
+    board = []
 
-for _ in range(n):
-    board.append(list(map(int, input())))
-    
-    
-def bfs(y, x):
+    for _ in range(n):
+        line = input().strip()
+        board.append([int(char) for char in line])
+
+    return n, m, board
+
+def is_valid_position(x, y, n, m):
+    return 0 < x <= n and 0 < y <= m
+
+def get_neighbors(x, y, n, m):
+    directions = [(-1, 0), (0, 1), (1, 0), (0, -1)]
+    neighbors = []
+
+    for dx, dy in directions:
+        next_x, next_y = x + dx, y + dy
+        if is_valid_position(next_x, next_y, n, m):
+            neighbors.append((next_x, next_y))
+
+    return neighbors
+
+def bfs_shortest_path(board, start_x, start_y, end_x, end_y):
+    copy_board = copy.deepcopy(board)
+
     queue = deque()
-    queue.append((1, y, x))
-    
-    board[y][x] = 0
-    
+    queue.append((start_x, start_y))
+
     while queue:
-        cnt, y, x = queue.popleft()
+        cur_x, cur_y = queue.popleft()
 
-        if y == n-1 and x == m-1:
-            print(cnt)
-            break
-            
-        for i in range(4):
-            ny = y + dy[i]
-            nx = x + dx[i]
-            
-            if ny < 0 or ny >= n or nx < 0 or nx >= m:
-                continue
+        if cur_x == end_x and cur_y == end_y:
+            return copy_board[cur_x-1][cur_y-1]
 
-            if board[ny][nx] == 1:
-                queue.append((cnt+1, ny, nx))
-                board[ny][nx] = 0
-                
-bfs(0, 0)
+        for next_x, next_y in get_neighbors(cur_x, cur_y, end_x, end_y):
+            if  copy_board[next_x-1][next_y-1] == 1:
+                copy_board[next_x-1][next_y-1] = copy_board[cur_x-1][cur_y-1] + 1
+
+                queue.append((next_x, next_y))
+
+def solve():
+    n, m, board = read_input()
+
+    result = bfs_shortest_path(board, 1, 1, n, m)
+
+    print(result)
+
+
+solve()
