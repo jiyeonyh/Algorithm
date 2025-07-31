18b5765ef46259cf5fb5c8ea166602879e0377ce
jiyeonyh <56222489+jiyeonyh@users.noreply.github.com>
2025-07-31 11:18:42 +0900

    [Gold IV] Title: 문제 추천 시스템 Version 1, Time: 236 ms, Memory: 60952 KB -BaekjoonHub

---
+++ 백준/Gold/21939. 문제 추천 시스템 Version 1/문제 추천 시스템 Version 1.py
@@ -0,0 +1,37 @@
+import sys
+import heapq
+from collections import defaultdict
+input = sys.stdin.readline
+
+n = int(input())
+max_heap = []
+min_heap = []
+solved = defaultdict(int)
+for _ in range(n):
+    p, l = map(int, input().split())
+    heapq.heappush(max_heap, (-l, -p))
+    heapq.heappush(min_heap, (l, p))
+
+m = int(input())
+for _ in range(m):
+    command = input().split()
+    if command[0] == "add":
+        l = int(command[2])
+        p = int(command[1])
+        heapq.heappush(max_heap, (-l, -p))
+        heapq.heappush(min_heap,  (l, p))
+    elif command[0] == "recommend":
+        if command[1] == '1':
+            while solved[-max_heap[0][1]] != 0:
+                solved[-max_heap[0][1]] -= 1 
+                heapq.heappop(max_heap)
+            print(-max_heap[0][1])
+        else:
+            while solved[min_heap[0][1]] != 0:
+                solved[min_heap[0][1]] -= 1
+                heapq.heappop(min_heap)
+            print(min_heap[0][1])
+    elif command[0] == "solved":
+        solved[int(command[1])] = True
+    
+
