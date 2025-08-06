ddb70e0bd91ab2e9699467c574c775c1f910c095
jiyeonyh <56222489+jiyeonyh@users.noreply.github.com>
2025-08-06 20:16:21 +0900

    [Silver V] Title: 비밀번호 발음하기, Time: 120 ms, Memory: 15804 KB -BaekjoonHub

--- 백준/Silver/4659. 비밀번호 발음하기/비밀번호 발음하기.java
+++ 백준/Silver/4659. 비밀번호 발음하기/비밀번호 발음하기.java
@@ -1,54 +1,55 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Main{
+    private static final Set<Character> VOWELS = new HashSet<>(Arrays.asList('a', 'e', 'i', 'o', 'u'));
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        List<Character> vowel = new ArrayList<>(Arrays.asList('a', 'e', 'i', 'o', 'u'));
-
         String line;
-        while((line = br.readLine()) != null && !line.equals("end")) {
-            int vowelCount = 0;
-            int consonantCount = 0;
-            Boolean isContainVowel = false;
-            Boolean isAcceptable = true;
-            for(int i = 0; i < line.length(); i++) {
-                char c = line.charAt(i);
-                
-                if (!isContainVowel && vowel.contains(c)) {
-                    isContainVowel = true;
-                }
 
-                if (vowel.contains(c)){
-                    vowelCount++;
-                    consonantCount = 0;
-                } else {
-                    consonantCount++;
-                    vowelCount = 0;
-                }
-                
-                if (c != 'e' && c != 'o' && i+1 < line.length()){
-                    char next = line.charAt(i+1);
-                    if (c == next) {
-                        isAcceptable = false;
-                        break;
-                    }
-                }
-                if (vowelCount >= 3 || consonantCount >= 3) {
-                    isAcceptable = false;
-                    break;
-                }
-            }
-            if (!isAcceptable || !isContainVowel) {
-                System.out.println("<" + line + "> is not acceptable.");
-            } else {
+        while((line = br.readLine()) != null && !line.equals("end")) {
+            if (isAcceptable(line)) {
                 System.out.println("<" + line + "> is acceptable.");
+            } else {
+                System.out.println("<" + line + "> is not acceptable.");
+            }
+        }
+        br.close();
+    }
+
+    private static boolean isAcceptable(String word) {
+        boolean hasVowel = false;
+        int vowelStreak = 0;
+        int consonantStreak = 0;
+
+        for (int i = 0; i < word.length(); i++) {
+            char current = word.charAt(i);
+            boolean isVowel = VOWELS.contains(current);
+
+            if (isVowel) {
+                hasVowel = true;
+                vowelStreak++;
+                consonantStreak = 0;
+            } else {
+                consonantStreak++;
+                vowelStreak = 0;
+            }
+
+            if (vowelStreak >= 3 || consonantStreak >= 3) {
+                return false;
+            }
+
+            if (i + 1 < word.length()) {
+                char next = word.charAt(i + 1);
+                if (current == next && !(current == 'e' || current == 'o')) {
+                    return false;
+                }
             }
         }
 
-        br.close();
+        return hasVowel;
     }
 }
